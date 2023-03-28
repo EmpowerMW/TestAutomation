0475bcfb2b953627a039f140f5df81726fd076f6
manuel wals <manuelwals@hotmail.com>
2023-03-28 16:25:44 +0200

    use order package

--- TestAutomationSolution_1/TestAutomationSolution_1.cs
+++ TestAutomationSolution_1/TestAutomationSolution_1.cs
@@ -55,6 +55,7 @@ namespace TestAutomationSolution_1
 	using System.Collections.Generic;
 	using System.Globalization;
 	using System.Text;
+	using Empower.Library.Room4;
 	using Skyline.DataMiner.Automation;
 	using Skyline.DataMiner.Core.DataMinerSystem.Automation;
 
@@ -69,11 +70,14 @@ namespace TestAutomationSolution_1
 		/// <param name="engine">Link with SLAutomation process.</param>
 		public void Run(IEngine engine)
 		{
-			var dms = engine.GetDms();
-			foreach (var element in dms.GetElements())
-			{
-				engine.GenerateInformation("element" + element.Name);
-			}
+			//var dms = engine.GetDms();
+			//foreach (var element in dms.GetElements())
+			//{
+			//	engine.GenerateInformation("element" + element.Name);
+			//}
+
+			var order = OrderFactory.CreateOrder("order 1");
+			order.Dispatch("Manuel Wals");
 		}
 	}
 }
