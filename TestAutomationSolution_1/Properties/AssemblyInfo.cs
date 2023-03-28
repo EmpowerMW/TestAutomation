4443ea9e4711d55d11c12184d141dd1bafec4461
manue <manue@DESKTOP-D7TK1BO>
2023-03-28 14:11:32 +0200

    Add project files.

---
+++ TestAutomationSolution_1/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("TestAutomationSolution_1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Skyline Communications")]
+[assembly: AssemblyProduct("TestAutomationSolution_1")]
+[assembly: AssemblyCopyright("Copyright © Skyline Communications")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("153DEDFA-0B9F-4DDE-82EE-3DB185168339")]
+[assembly: AssemblyVersion("1.0.0.0")]
